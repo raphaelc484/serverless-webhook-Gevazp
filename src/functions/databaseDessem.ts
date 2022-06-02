2752534a9dc14df2247e0ff20a5aed28bd7ce980
Raphael <raphaelc484@hotmail.com>
2022-06-02 11:22:39 -0300

    up

--- src/functions/databaseDessem.ts
+++ src/functions/databaseDessem.ts
@@ -50,132 +50,136 @@ export const handler: S3Handler = async (event) => {
     .getObject({ Bucket: bucketName, Key: fileKey })
     .promise();
 
-  // if (dataRead) {
-  //   try {
-  //     const prisma = new PrismaClient();
+  if (dataRead) {
+    try {
+      const arquivo = dataRead.Body.toString("utf-8");
+      const testeLeitura = fs.readFileSync(arquivo, "utf-8").split("\n");
+      console.log(testeLeitura);
 
-  //     const findLastDateDocType = await prisma.tbl_file_data.findFirst({
-  //       where: {
-  //         nom_file: docType,
-  //       },
-  //       orderBy: { num_file: "desc" },
-  //     });
+      //     const prisma = new PrismaClient();
 
-  //     const findLastDataInSystem = await prisma.tbl_load_daily.findMany({
-  //       where: {
-  //         dat_running: findLastDateDocType.dat_file_publi,
-  //       },
-  //     });
+      //     const findLastDateDocType = await prisma.tbl_file_data.findFirst({
+      //       where: {
+      //         nom_file: docType,
+      //       },
+      //       orderBy: { num_file: "desc" },
+      //     });
 
-  //     const read = fs
-  //       .readFileSync(dataRead.Body.toString("utf-8"), "utf-8")
-  //       .split("\n");
+      //     const findLastDataInSystem = await prisma.tbl_load_daily.findMany({
+      //       where: {
+      //         dat_running: findLastDateDocType.dat_file_publi,
+      //       },
+      //     });
 
-  //     const lines = read.filter((line) => line.includes("DP"));
+      //     const read = fs
+      //       .readFileSync(dataRead.Body.toString("utf-8"), "utf-8")
+      //       .split("\n");
 
-  //     const lineValues = [];
-  //     let lineSplit;
+      //     const lines = read.filter((line) => line.includes("DP"));
 
-  //     for (let i = 0; i < lines.length; i += 1) {
-  //       lineSplit = lines[i].split(" ").filter((l) => l !== " " && l !== "");
-  //       if (lineSplit[2] === "11") {
-  //         lineValues.push({
-  //           ss: lineSplit[1],
-  //           di: lineSplit[2],
-  //           h1: lineSplit[3],
-  //           m: lineSplit[4],
-  //           carga: lineSplit[6],
-  //         });
-  //       } else {
-  //         lineValues.push({
-  //           ss: lineSplit[1],
-  //           di: lineSplit[2],
-  //           h1: lineSplit[3],
-  //           m: lineSplit[4],
-  //           carga: lineSplit[6],
-  //         });
-  //       }
-  //     }
-  //     const tableFromTxt = [];
+      //     const lineValues = [];
+      //     let lineSplit;
 
-  //     for (let x = 1; x <= 4; x += 1) {
-  //       const subsystemOne = lineValues.filter((r) => r.ss === x.toString());
+      //     for (let i = 0; i < lines.length; i += 1) {
+      //       lineSplit = lines[i].split(" ").filter((l) => l !== " " && l !== "");
+      //       if (lineSplit[2] === "11") {
+      //         lineValues.push({
+      //           ss: lineSplit[1],
+      //           di: lineSplit[2],
+      //           h1: lineSplit[3],
+      //           m: lineSplit[4],
+      //           carga: lineSplit[6],
+      //         });
+      //       } else {
+      //         lineValues.push({
+      //           ss: lineSplit[1],
+      //           di: lineSplit[2],
+      //           h1: lineSplit[3],
+      //           m: lineSplit[4],
+      //           carga: lineSplit[6],
+      //         });
+      //       }
+      //     }
+      //     const tableFromTxt = [];
 
-  //       for (let i = 0; i < subsystemOne.length; i += 1) {
-  //         const j = i + 1;
+      //     for (let x = 1; x <= 4; x += 1) {
+      //       const subsystemOne = lineValues.filter((r) => r.ss === x.toString());
 
-  //         if (j < subsystemOne.length) {
-  //           const dataTest = {
-  //             dat_running: findLastDateDocType,
-  //             dat_initi: ale(
-  //               subsystemOne[i].di,
-  //               subsystemOne[i].h1,
-  //               subsystemOne[i].m
-  //             ),
-  //             dat_final: ale(
-  //               subsystemOne[j].di,
-  //               subsystemOne[j].h1,
-  //               subsystemOne[j].m
-  //             ),
-  //             val_load_daily: parseFloat(subsystemOne[i].carga),
-  //             num_ssis: parseFloat(subsystemOne[i].ss),
-  //           };
-  //           tableFromTxt.push(dataTest);
-  //         } else {
-  //           const dataTest = {
-  //             dat_running: findLastDateDocType,
-  //             dat_initi: ale(
-  //               subsystemOne[i].di,
-  //               subsystemOne[i].h1,
-  //               subsystemOne[i].m
-  //             ),
-  //             dat_final: ale(
-  //               (parseFloat(subsystemOne[i].di) + 1).toString(),
-  //               "0",
-  //               "0"
-  //             ),
-  //             val_load_daily: parseFloat(subsystemOne[i].carga),
-  //             num_ssis: parseFloat(subsystemOne[i].ss),
-  //           };
-  //           tableFromTxt.push(dataTest);
-  //         }
-  //       }
-  //     }
+      //       for (let i = 0; i < subsystemOne.length; i += 1) {
+      //         const j = i + 1;
 
-  //     if (findLastDataInSystem.length < 1) {
-  //       console.log("estamos aqui,create");
+      //         if (j < subsystemOne.length) {
+      //           const dataTest = {
+      //             dat_running: findLastDateDocType,
+      //             dat_initi: ale(
+      //               subsystemOne[i].di,
+      //               subsystemOne[i].h1,
+      //               subsystemOne[i].m
+      //             ),
+      //             dat_final: ale(
+      //               subsystemOne[j].di,
+      //               subsystemOne[j].h1,
+      //               subsystemOne[j].m
+      //             ),
+      //             val_load_daily: parseFloat(subsystemOne[i].carga),
+      //             num_ssis: parseFloat(subsystemOne[i].ss),
+      //           };
+      //           tableFromTxt.push(dataTest);
+      //         } else {
+      //           const dataTest = {
+      //             dat_running: findLastDateDocType,
+      //             dat_initi: ale(
+      //               subsystemOne[i].di,
+      //               subsystemOne[i].h1,
+      //               subsystemOne[i].m
+      //             ),
+      //             dat_final: ale(
+      //               (parseFloat(subsystemOne[i].di) + 1).toString(),
+      //               "0",
+      //               "0"
+      //             ),
+      //             val_load_daily: parseFloat(subsystemOne[i].carga),
+      //             num_ssis: parseFloat(subsystemOne[i].ss),
+      //           };
+      //           tableFromTxt.push(dataTest);
+      //         }
+      //       }
+      //     }
 
-  //       await prisma.tbl_load_daily.createMany({
-  //         data: tableFromTxt,
-  //       });
-  //     } else {
-  //       console.log("estamos aqui,update");
+      //     if (findLastDataInSystem.length < 1) {
+      //       console.log("estamos aqui,create");
 
-  //       for (let z = 0; z < findLastDataInSystem.length; z += 1) {
-  //         await prisma.tbl_load_daily.update({
-  //           where: {
-  //             num_load_daily: findLastDataInSystem[z].num_load_daily,
-  //           },
-  //           data: {
-  //             dat_initi: tableFromTxt[z].dat_initi,
-  //             dat_final: tableFromTxt[z].dat_final,
-  //             dat_running: tableFromTxt[z].dat_running,
-  //             val_load_daily: tableFromTxt[z].val_load_daily,
-  //             num_ssis: tableFromTxt[z].num_ssis,
-  //           },
-  //         });
-  //       }
-  //     }
+      //       await prisma.tbl_load_daily.createMany({
+      //         data: tableFromTxt,
+      //       });
+      //     } else {
+      //       console.log("estamos aqui,update");
 
-  //     // console.log("teste", tableFromTxt);
+      //       for (let z = 0; z < findLastDataInSystem.length; z += 1) {
+      //         await prisma.tbl_load_daily.update({
+      //           where: {
+      //             num_load_daily: findLastDataInSystem[z].num_load_daily,
+      //           },
+      //           data: {
+      //             dat_initi: tableFromTxt[z].dat_initi,
+      //             dat_final: tableFromTxt[z].dat_final,
+      //             dat_running: tableFromTxt[z].dat_running,
+      //             val_load_daily: tableFromTxt[z].val_load_daily,
+      //             num_ssis: tableFromTxt[z].num_ssis,
+      //           },
+      //         });
+      //       }
+      //     }
 
-  //     // await MicrosoftTeam({
-  //     //   title: "Previsão do Dessem",
-  //     //   message:
-  //     //     "Dados diários do Dessem, clique no link para acessar o relatório",
-  //     // });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+      //     // console.log("teste", tableFromTxt);
+
+      //     // await MicrosoftTeam({
+      //     //   title: "Previsão do Dessem",
+      //     //   message:
+      //     //     "Dados diários do Dessem, clique no link para acessar o relatório",
+      //     // });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
